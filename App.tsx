e12831859f3b3c77380d3b38e57eb11da3f1801e
rehan ismail <rehanismail.dev@gmail.com>
2025-04-30 01:27:40 +0500

    `Refactor App.tsx and update package dependencies`

--- App.tsx
+++ App.tsx
@@ -1,131 +1,85 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, {useEffect, useState} from 'react';
+import {Button, TextInput, View, Text} from 'react-native';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+const App = () => {
+  const [data, setData] = useState([]);
+  const fetchData = async () => {
+    const url = 'http://192.168.2.203:3000/users';
+    let result = await fetch(url);
+    result = await result.json();
+    if (result) {
+      setData(result);
+    }
+    
   };
 
-  /*
-   * To keep the template simple and small we're adding padding to prevent view
-   * from rendering under the System UI.
-   * For bigger apps the reccomendation is to use `react-native-safe-area-context`:
-   * https://github.com/AppAndFlow/react-native-safe-area-context
-   *
-   * You can read more about it here:
-   * https://github.com/react-native-community/discussions-and-proposals/discussions/827
-   */
-  const safePadding = '5%';
+  const deleteData = async (id) => {
+    const url = 'http://192.168.2.203:3000/users';
+    const result = await fetch(`${url}/${id}`, {
+      method: 'Delete',
+      headers: {'Content-Type': 'Application/json'},
+    });
+    fetchData()
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
-    <View style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        style={backgroundStyle}>
-        <View style={{paddingRight: safePadding}}>
-          <Header/>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: 'white',
+      }}>
+      <View
+        style={{
+          flexDirection: 'row',
+          alignItems: 'center',
+          backgroundColor: 'orange',
+          margin: 5,
+          padding: 8,
+          height: '6%',
+        }}>
+        <View style={{flex: 1}}>
+          <Text style={{fontWeight: '900'}}>name</Text>
         </View>
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-            paddingHorizontal: safePadding,
-            paddingBottom: safePadding,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+        <View style={{flex: 1}}>
+          <Text style={{fontWeight: '900'}}>age</Text>
         </View>
-      </ScrollView>
+        <View style={{flex: 1.3}}>
+          <Text style={{fontWeight: '900'}}>operation</Text>
+        </View>
+      </View>
+      {data.length
+        ? data.map(item => (
+            <View
+              key={item.id}
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                backgroundColor: 'orange',
+                margin: 5,
+                padding: 8,
+              }}>
+              <View style={{flex: 1}}>
+                <Text>{item.name}</Text>
+              </View>
+              <View style={{flex: 1}}>
+                <Text>{item.age}</Text>
+              </View>
+              <View style={{flexDirection: 'row', gap: 8}}>
+                <View>
+                  <Button title="Update" />
+                </View>
+                <View>
+                  <Button title="Delete" onPress={()=>{deleteData(item.id)}}/>
+                </View>
+              </View>
+            </View>
+          ))
+        : null}
     </View>
   );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+};
 
 export default App;
