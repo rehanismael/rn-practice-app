d7df10b5726d837e4a15670171abcb9cb217a7c5
rehan ismail <rehanismail.dev@gmail.com>
2025-05-20 11:09:12 +0500

    Rename project to rn-foundations

--- App.tsx
+++ App.tsx
@@ -1,85 +1,26 @@
-import React, {useEffect, useState} from 'react';
-import {Button, TextInput, View, Text} from 'react-native';
-
+import React, {useState} from 'react';
+import {View, Text, TextInput,Button} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage'
 const App = () => {
-  const [data, setData] = useState([]);
-  const fetchData = async () => {
-    const url = 'http://192.168.2.203:3000/users';
-    let result = await fetch(url);
-    result = await result.json();
-    if (result) {
-      setData(result);
-    }
-    
-  };
 
-  const deleteData = async (id) => {
-    const url = 'http://192.168.2.203:3000/users';
-    const result = await fetch(`${url}/${id}`, {
-      method: 'Delete',
-      headers: {'Content-Type': 'Application/json'},
-    });
-    fetchData()
-  };
-  useEffect(() => {
-    fetchData();
-  }, []);
+  const setStoredData = async ()=> {
+   await AsyncStorage.setItem("name","rehan");
+  }
+  const getStoredData =async ()=> {
+   const name = await AsyncStorage.getItem("name");
+    console.warn(name);
+  }
+ 
+
 
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: 'white',
-      }}>
-      <View
-        style={{
-          flexDirection: 'row',
-          alignItems: 'center',
-          backgroundColor: 'orange',
-          margin: 5,
-          padding: 8,
-          height: '6%',
-        }}>
-        <View style={{flex: 1}}>
-          <Text style={{fontWeight: '900'}}>name</Text>
-        </View>
-        <View style={{flex: 1}}>
-          <Text style={{fontWeight: '900'}}>age</Text>
-        </View>
-        <View style={{flex: 1.3}}>
-          <Text style={{fontWeight: '900'}}>operation</Text>
-        </View>
-      </View>
-      {data.length
-        ? data.map(item => (
-            <View
-              key={item.id}
-              style={{
-                flexDirection: 'row',
-                alignItems: 'center',
-                backgroundColor: 'orange',
-                margin: 5,
-                padding: 8,
-              }}>
-              <View style={{flex: 1}}>
-                <Text>{item.name}</Text>
-              </View>
-              <View style={{flex: 1}}>
-                <Text>{item.age}</Text>
-              </View>
-              <View style={{flexDirection: 'row', gap: 8}}>
-                <View>
-                  <Button title="Update" />
-                </View>
-                <View>
-                  <Button title="Delete" onPress={()=>{deleteData(item.id)}}/>
-                </View>
-              </View>
-            </View>
-          ))
-        : null}
-    </View>
+
+ <View>
+    <Button title='Set Data' onPress={setStoredData}/>
+    <Button title='Get Data' onPress={getStoredData}/>
+
+
+  </View>
   );
 };
-
 export default App;
